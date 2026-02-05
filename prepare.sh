ed8e08b9ebd97d24992f4010d8c56a958136c2b2
Claire <claire@lemoncloud.io>
2026-02-05 15:52:19 +0900

    feat: add functionality to copy pages folder in prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -88,6 +88,11 @@ if [ -d "$APP_DIR/utils" ]; then
   echo "Copy utils folder successful!"
   ls "$FRONTEN_SOURCE/utils" 2>/dev/null || echo "WARN! Copy to frontend/utils failed."
 fi
+if [ -d "$APP_DIR/pages" ]; then
+  cp -rf "$APP_DIR/pages/" "./$FRONTEN_SOURCE/pages/"
+  echo "Copy pages folder successful!"
+  ls "$FRONTEN_SOURCE/pages" 2>/dev/null || echo "WARN! Copy to frontend/pages failed."
+fi
 if [ -f "$APP_DIR/App.tsx" ]; then
   cp -rf "$APP_DIR/App.tsx" "./$FRONTEN_SOURCE/"
   echo "Copy App.tsx successful!"
