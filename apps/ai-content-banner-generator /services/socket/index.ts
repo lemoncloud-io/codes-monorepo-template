6ea84c4d29a9390d84f90a435206ac5ca0c965a1
Steve Jung <steve@lemoncloud.io>
2026-06-18 17:39:01 +0900

    chore: cleanup

--- apps/ai-content-banner-generator /services/socket/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * `socket/index.ts`
- * - socket simulation utilities.
- *
- * @copyright (C) 2026 LemonCloud Co Ltd. - All Rights Reserved.
- */
-export * from './types';
-export * from './socket';
-export * from './transport';
